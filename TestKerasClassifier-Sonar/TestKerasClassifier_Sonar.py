6e20eca75b132d19c4dc72c9dded6d860b5b51aa
Piotr Falkowski <falcon777@wp.pl>
2019-06-12 21:28:58 +0200

    Refactor

--- TestKerasClassifier-Sonar/TestKerasClassifier_Sonar.py
+++ TestKerasClassifier-Sonar/TestKerasClassifier_Sonar.py
@@ -10,55 +10,57 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 
-    # baseline model
-def create_baseline():
-	# create model
-	model = Sequential()
-	model.add(Dense(60, input_dim=60, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
-	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+class ClassificationApi:
+    
+    def __init__(self):
+        return
 
-# smaller model
-def create_smaller():
-	# create model
-	model = Sequential()
-	model.add(Dense(30, input_dim=60, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
-	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+    def inputLen(self):
+        return self.dataframe.shape[1] - 1
 
-# larger model
-def create_larger():
-	# create model
-	model = Sequential()
-	model.add(Dense(60, input_dim=60, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(30, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
-	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+    def ReadData(self, path):
+        self.dataframe = pandas.read_csv(path, header=None)
+        self.dataset = self.dataframe.values
+        self.X = self.dataset[:,0:self.inputLen()].astype(float)
+        self.Y = self.dataset[:,self.inputLen()]
+
+    def EncodeY(self):
+        encoder = LabelEncoder()
+        encoder.fit(self.Y)
+        self.encoded_Y = encoder.transform(self.Y)
+
+    def CreateModel(self):
+        if (self.dataframe is None):
+            raise ValueError('dataframe is not initialized. Use ReadData() before calling CreateModel()')
+        model = Sequential()
+        model.add(Dense(self.inputLen(), input_dim = self.inputLen(), kernel_initializer='normal', activation='relu'))
+        model.add(Dense(30, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))	    
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+    def TrainModel(self, iterations = 100, batchSize = 5):
+        if (self.dataframe is None):
+            raise ValueError('dataframe is not initialized. Use ReadData() before calling TrainModel()')
+        estimators = []
+        estimators.append(('standardize', StandardScaler()))
+        estimators.append(('mlp', KerasClassifier(build_fn=self.CreateModel, epochs=iterations, batch_size=batchSize, verbose=0)))
+        pipeline = Pipeline(estimators)
+        kfold = StratifiedKFold(n_splits=10, shuffle=True)
+        self.results = cross_val_score(pipeline, self.X, self.encoded_Y, cv=kfold)
+        return self.results
+
+    def GetSummary(self):
+        if (self.results is None):
+            raise ValueError('results are None. Use TrainModel() before calling GetSummary()')
+        return f"Standardized: {self.results.mean()*100} ({self.results.std()*100})"
 
 if __name__ == '__main__':
     # load dataset
-    dataframe = pandas.read_csv("..\sonar.csv", header=None)
-    dataset = dataframe.values
-    # split into input (X) and output (Y) variables
-    X = dataset[:,0:60].astype(float)
-    Y = dataset[:,60]
-
-    # encode class values as integers
-    encoder = LabelEncoder()
-    encoder.fit(Y)
-    encoded_Y = encoder.transform(Y)
-
+    api = ClassificationApi()
+    api.ReadData("..\sonar.csv")
+    api.EncodeY()
+    api.CreateModel()
+    api.TrainModel()
+    print(api.GetSummary())
     # evaluate baseline model with standardized dataset
-    estimators = []
-    estimators.append(('standardize', StandardScaler()))
-    estimators.append(('mlp', KerasClassifier(build_fn=create_smaller, epochs=100, batch_size=5, verbose=0)))
-    pipeline = Pipeline(estimators)
-    kfold = StratifiedKFold(n_splits=10, shuffle=True)
-    results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
-    print("Standardized: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
